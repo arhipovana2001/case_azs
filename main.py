a1f0542f60c35d24b40eedebbb59e5a956725ea9
Nastya Arkhipova <55373084+arhipovana2001@users.noreply.github.com>
2020-04-17 00:19:50 +0700

    unpack

--- main.py
+++ main.py
@@ -27,3 +27,36 @@ bnz_price = {}
 digits = '0123456789'
 left = ''
 all_text = ''
+
+
+def convertation(lst):
+    s = ''
+    for i in lst:
+        i = str(i)
+        s = s + i + ', '
+    return s[:-2]
+
+
+with open("input.txt", encoding='utf-8') as inp:
+    for i in inp:
+        line = i.split()
+        time_2[line[0]] = line[-1]
+        time_3[line[0]] = line[1]
+        time_1[line[2]] = time_1.get(line[2], 0) + int(line[1])
+
+with open("azs.txt", encoding='utf-8') as azs:
+    for i in azs:
+        line = i.split()
+        queue[line[0]] = line[1]
+        bnz_reverse[line[0]] = line[2:]
+        for j in line:
+            if j not in digits:
+                if j not in bnz_set:
+                    bnz_set[j] = [line[0]]
+                else:
+                    bnz_set[j] += line[0]
+
+with open("price.txt", encoding='utf-8') as price:
+    for i in price:
+        prc = i.split()
+        bnz_price[prc[0]] = int(prc[-1])
